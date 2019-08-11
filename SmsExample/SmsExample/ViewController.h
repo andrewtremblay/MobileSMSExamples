31505bfd4b3c7f4003cd83fe97bc47bc8de95c92
Andrew Tremblay <gloriousrocketship@gmail.com>
2019-08-11 12:01:43 -0400

    gitignore files

---
+++ SmsExample/SmsExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmsExample
+//
+//  Created by Andrew Tremblay on 8/11/19.
+//  Copyright © 2019 Andrew Tremblay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SmsExample/SmsExample/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SmsExample
+//
+//  Created by Andrew Tremblay on 8/11/19.
+//  Copyright © 2019 Andrew Tremblay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
